d757e8c79ad3b347be1d069f239b1bd1618b53ce
gakukek <darrylclaylayaya@gmail.com>
2025-12-21 14:21:21 +0700

    why

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const VITE_CLERK_PUBLISHABLE_KEY="pk_live_Y2xlcmsuY2U3MzktZmUucGFnZXMuZGV2JA";
+{
+  const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+  const frontendApi = import.meta.env.VITE_CLERK_FRONTEND_API || undefined;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={VITE_CLERK_PUBLISHABLE_KEY}
+    <ClerkProvider publishableKey={publishableKey}
       appearance={{ layout: { unsafe_disableDevelopmentModeWarnings: true } }}>
       <App />
       <Toaster position="top-right" />
