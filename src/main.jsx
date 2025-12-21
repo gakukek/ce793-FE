f96cb62aa99fd81ddfbbf24e29964187f971c30f
gakukek <darrylclaylayaya@gmail.com>
2025-12-21 14:36:17 +0700

    endless debugging

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
+console.log("CLERK KEY:", import.meta.env.VITE_CLERK_PUBLISHABLE_KEY);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
