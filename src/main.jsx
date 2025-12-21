2da339842510b2fffa24556f1a19fc1668ff2042
gakukek <darrylclaylayaya@gmail.com>
2025-12-21 13:14:49 +0700

    remove key

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,12 @@ import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { ClerkProvider } from "@clerk/clerk-react";
 
-const VITE_CLERK_PUBLISHABLE_KEY="pk_live_Y2xlcmsuY2U3MzktZmUucGFnZXMuZGV2JA"
+const VITE_CLERK_PUBLISHABLE_KEY =
+  import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!VITE_CLERK_PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk publishable key");
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
