94d77345a4be974d962fa3581a346c8cac1c7e6b
gakukek <darrylclaylayaya@gmail.com>
2025-12-21 13:19:51 +0700

    debugging

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,10 @@ const VITE_CLERK_PUBLISHABLE_KEY =
   import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!VITE_CLERK_PUBLISHABLE_KEY) {
+  VITE_CLERK_PUBLISHABLE_KEY="pk_live_Y2xlcmsuY2U3MzktZmUucGFnZXMuZGV2JA";
+  console.log(VITE_CLERK_PUBLISHABLE_KEY);
   throw new Error("Missing Clerk publishable key");
+
 }
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -22,4 +25,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </React.StrictMode>
 );
 
-// ini anejh
